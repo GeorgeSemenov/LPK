ed519eedff0a9ec94b0ad4ab0f095c4d3038e10b
Семенов <@RIB.local>
2024-03-06 12:15:49 +0300

    add form and elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import {
+  Button,
+  FormControl,
+  FormGroup,
+  Input,
+  InputLabel,
+  Typography,
+} from "@mui/material";
 
 function App() {
+  const [gilsaInput, setGilsaInput] = useState("");
+  const [siemInput, setSiemInput] = useState("8400");
+  const [isError, setIsError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(
+    "Данные введены не корректно, проверьте."
+  );
+  function btnClick() {}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FormGroup sx={{ padding: "15px" }}>
+      {isError && <Typography>{errorMsg}</Typography>}
+      <FormControl sx={{ marginBottom: "15px" }}>
+        <InputLabel htmlFor="gilsaInput">Длины гильз</InputLabel>
+        <Input
+          placeholder="Введи данные через пробел"
+          id="gilsaInput"
+          onChange={(e) => {
+            setGilsaInput(e.target.value);
+          }}
+          value={gilsaInput}
+        />
+      </FormControl>
+      <FormControl sx={{ marginBottom: "15px" }}>
+        <InputLabel htmlFor="siemInput">Длина сьёма</InputLabel>
+        <Input
+          placeholder="8400"
+          id="siemInput"
+          onChange={(e) => {
+            setSiemInput(e.target.value);
+          }}
+          value={siemInput}
+        />
+      </FormControl>
+      <Button
+        variant="contained"
+        sx={{ marginBottom: "15px" }}
+        onClick={btnClick}
+      >
+        Найти подходящую сумму
+      </Button>
+      <Typography component={"h3"} textAlign={"center"}>
+        Тут ведут расчёт суровые работники лесопромышленной области.
+      </Typography>
+      <img
+        alt="Знойный лесоруб"
+        src="https://www.veseloeradio.ru/vardata/modules/news/files/1/2453/news_file_2453_599fca86a29b4.jpg"
+        width="100%"
+      />
+    </FormGroup>
   );
 }
 
